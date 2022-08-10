3ae4687c64e1c0513392f077e28d73648abed06b
rjm <rjmatt77@googlemail.com>
2022-08-10 19:17:01 +0100

    remove legacy

--- src/main/scala/Traversal.scala
+++ src/main/scala/Traversal.scala
@@ -6,9 +6,6 @@ import Parser.getLinks;
 
 object Traversal extends Thread{
   def traverseSite(root: String) = {
-    // val s: Stack[Page] = new Stack()
-    // val entry = Page(root, findLinks(root, root))
-
     val dfsSolution = dfs(root)
     dfsSolution.map(l => println(s"${l}\n"))
     println(s"${dfsSolution.length} :: ${dfsSolution.distinct.length}")
@@ -31,31 +28,5 @@ object Traversal extends Thread{
   }
 
 
-
-  // @deprecated
-  // def t(root: String, stack: Stack[Page], curPage: Page, acc: List[String]): List[String] = {
-  //   if (logging(stackTrace)) println(s"t :: ${stack.size} :: ${curPage.thisPage} :: ${curPage.unvisitedLinks.size} :: ${acc.length}")
-    
-  //   if (isExplored(curPage)) {
-  //     stack.pop()
-  //     if (stack.isEmpty) acc
-  //     else {
-  //       trimStackHead(stack,acc)
-  //       if (logging(deepTrace)) print("Calling from A :: ")
-  //       t(root, stack, stack.head, acc)
-  //     }
-  //   } else {
-  //     val trimmedPage = getUnvisited(curPage, acc)
-  //     nextPage(trimmedPage) match {
-  //       case Some(nextPageStr) => 
-  //         val newPage = GetNextPage(root, nextPageStr)
-  //         if (logging(deepTrace)) print("Calling from B :: ")
-  //         t(root, stack.push(newPage), newPage, acc.prepended(newPage.thisPage))
-  //       case None => 
-  //         if (logging(deepTrace)) print("Calling from C :: ")
-  //         t(root, stack, trimmedPage, acc)
-  //     }
-  //   }
-  // }
 }
 
